b92910461aafc9f0f9e976e1cd1311837df4deef
Yoon-Suji <jidl88@ewhain.net>
2022-07-28 11:13:17 +0900

    [2-1] add CONNECT and DISCONNECT button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import "./App.css";
 import React from "react";
+import { login, logout } from "./near/utils";
 
 function App() {
-  return <div className="App"></div>;
+  const account = window.accountId;
+
+  const renderBtn = () => {
+    if (account) {
+      return (
+        <button type="button" onClick={logout} className="disconnect-btn">
+          DISCONNECT
+        </button>
+      );
+    }
+    return (
+      <button type="button" onClick={login} className="connect-btn">
+        CONNECT WALLET
+      </button>
+    );
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <div className="header-titles">
+          <img
+            src={process.env.PUBLIC_URL + "/near_logo.svg"}
+            alt="NEAR Logo"
+            className="near-logo"
+            width="500"
+          />
+          <h1>Clicker Game</h1>
+        </div>
+      </header>
+      <div className="App-container">
+        <div className="connect-wallet">{renderBtn()}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { initContract, reset } from "./near/utils";
+import { initContract } from "./near/utils";
 
 initContract().then(() => {
-  // (debug)
-  reset();
-
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
     <React.StrictMode>
